cf99e7fabbedebf1c74b3bc2370c02e7bca85bb2
harry-the-captain <2022bit064@sggs.ac.in>
2024-03-27 23:55:02 +0530

    toString

---
+++ quest6.java
@@ -0,0 +1,24 @@
+// toString method program
+
+class laptop
+{
+    String brand;
+    int price;
+
+    public String toString()
+    {
+        return "hi";
+    }
+
+}
+
+class quest6
+{
+    public static void main(String[] args)
+        {
+            laptop L = new laptop();
+           L.brand="LENOVO";
+           L.price = 35000;
+           System.out.println(L);
+        }
+    }
