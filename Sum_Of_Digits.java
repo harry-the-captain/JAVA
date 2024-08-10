833fcb1480f5110542ff27d188a716f3485911f2
harry-the-captain <155612183+harry-the-captain@users.noreply.github.com>
2024-08-10 19:40:47 +0530

    Create Sum_Of_Digits_(GFG).java

---
+++ Sum_Of_Digits.java
@@ -0,0 +1,10 @@
+# Read the input value for N
+N = int(input("Enter a number N: "))
+
+# Initialize the counter
+i = 1
+
+# Use a while loop to calculate and print the squares
+while i <= N:
+    print(i ** 2, end=' ')
+    i += 1
