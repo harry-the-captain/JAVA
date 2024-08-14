6d344aa88704f4f59d13af3fb4830be327a8ac2f
harry-the-captain <155612183+harry-the-captain@users.noreply.github.com>
2024-08-14 23:35:18 +0530

    Create Smallest_Element_In_Array.java

---
+++ Smallest_Element_In_Array.java
@@ -0,0 +1,22 @@
+public class Smallest_Element 
+
+{
+  public static void main(String args[])
+  {
+
+     int arr[] = {12, 13, 1, 10, 34, 10};
+
+     int min = arr[0];
+
+     for(int i=0; i<arr.length; i++)
+       { 
+         if(min > arr[i])
+         {
+            min = arr[i];
+         }
+
+     }
+
+     System.out.print(min); 
+  }
+}
