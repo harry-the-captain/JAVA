1230edae7af4c8e89066df1189e9c09713f5a841
tech_captain <155612183+harry-the-captain@users.noreply.github.com>
2024-09-19 00:53:35 +0530

    basic logical program.java

---
+++ logical.java
@@ -0,0 +1,8 @@
+class logical {
+    public static void main(String[] args) {
+        System.out.println('h'+'a'+'r'+'r'+'y');
+    }
+}
+
+// output: 550  
+// it add the all ASCII values of letters
